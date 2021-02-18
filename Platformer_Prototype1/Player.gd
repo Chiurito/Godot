d18f866a0deae9334b37d4d8ba96d9e38c058c3d
Chiurito <roc.belli@outlook.it>
2021-02-18 09:18:12 +0100

    .

--- Platformer_Prototype1/Player.gd
+++ Platformer_Prototype1/Player.gd
@@ -1,13 +1,22 @@
 extends KinematicBody2D
 
+const SPEED = 180
+const GRAVITY = 35
+const JUMP_FORCE = -1100
+
 var velocity: Vector2 = Vector2.ZERO
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
-		velocity.x = 100
+		velocity.x = SPEED
 	if Input.is_action_pressed("left"):
-		velocity.x = -100
+		velocity.x = -SPEED
+		
+	velocity.y = velocity.y + GRAVITY
 	
-	move_and_slide(velocity)
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = JUMP_FORCE
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	velocity.x = lerp(velocity.x, 0, 0.2)
