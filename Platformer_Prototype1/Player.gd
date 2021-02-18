8a7834183b472ec1fb19f34c4fea661f0bab6ee9
Chiurito <roc.belli@outlook.it>
2021-02-18 09:45:01 +0100

    Added animation

--- Platformer_Prototype1/Player.gd
+++ Platformer_Prototype1/Player.gd
@@ -9,9 +9,18 @@ var velocity: Vector2 = Vector2.ZERO
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	if Input.is_action_pressed("left"):
+		$Sprite.play("Walk")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$Sprite.play("Walk")
+		$Sprite.flip_h = true
+	else:
+		$Sprite.play("Idle")
 		
+	if !is_on_floor():
+		$Sprite.play("Air")
+	
 	velocity.y = velocity.y + GRAVITY
 	
 	if Input.is_action_just_pressed("jump") && is_on_floor():
